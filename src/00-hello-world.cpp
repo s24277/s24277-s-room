984a6a43a61204233e6bcf123c734421fa81c2d7
Karol Hulanicki <s24277@pjwstk.edu.pl>
2021-10-06 17:09:20 +0200

    test

---
+++ src/00-hello-world.cpp
@@ -0,0 +1,17 @@
+
+/*
+ * The very first program you should write in any new programming language.
+ */
+
+
+#include <iostream>
+
+
+auto main() -> int
+{
+    std::cout << "Hello, World!\n";
+std::cout << "Hello, Karol!";
+std::cout << "Hello, Charles!\n";
+
+  return 0;
+}
