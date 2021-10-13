532c02ada8aa173b67402944a6d084c65617b224
Karol Hulanicki <s24277@pjwstk.edu.pl>
2021-10-13 17:54:01 +0200

    password

---
+++ src/s02-password.cpp
@@ -0,0 +1,22 @@
+/*
+ * Ask the user for their credentials. Allow them to decline.
+ */
+
+#include <iostream>
+#include <string>
+
+auto main() -> int
+ {
+  std::cout << "Hasło: \n";
+
+  auto name = std::string{};
+  std::getline(std::cin, name);
+
+while (name != "student")
+{
+std::cout << "Hasło: \n";
+std::getline(std::cin, name);
+}
+ std::cout << "Pomyślnie zalogowano\n";
+  return 0;
+}
